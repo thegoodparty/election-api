f260e95485a4c1ec33bc745acb0bde921fa59611
oakinh <nikaohill@gmail.com>
2025-05-29 12:35:16 -0500

    Fix again

--- prisma/migrations/20250529173351_position_geoid/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `position_geoid` to the `Race` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Race" ADD COLUMN     "position_geoid" TEXT NOT NULL;
