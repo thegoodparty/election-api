bbee1a44230daaa7ebb5115a2e62396a00bcd493
oakinh <nikaohill@gmail.com>
2025-05-29 12:34:01 -0500

    Fix

--- prisma/migrations/20250529173351_position_geoid/migration.sql
+++ prisma/migrations/20250529173351_position_geoid/migration.sql
@@ -1,12 +1,8 @@
 /*
   Warnings:
 
-  - Made the column `position_name` on table `Candidacy` required. This step will fail if there are existing NULL values in that column.
   - Added the required column `position_geoid` to the `Race` table without a default value. This is not possible if the table is not empty.
 
 */
--- AlterTable
-ALTER TABLE "Candidacy" ALTER COLUMN "position_name" SET NOT NULL;
-
 -- AlterTable
 ALTER TABLE "Race" ADD COLUMN     "position_geoid" TEXT NOT NULL;
